428023de9a5037c68210fc4a5a75db0713fbff3e
murban11 <milosz.urbaniak02@gmail.com>
2024-03-31 18:47:30 +0200

    Add method for sign in

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
-from sqlalchemy import create_engine, exists
+from sqlalchemy import create_engine, exists, select
 from config import credentials
 from sqlalchemy.orm import Session
 from user import User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager
 
 app = Flask(__name__)
 engine = create_engine(credentials)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return get_user_by_id(user_id) 
 
 def add_user(nickname, password):
     with Session(engine) as session:
@@ -19,6 +26,19 @@ def add_user(nickname, password):
 def check_user_exists(nickname):
     with Session(engine) as session:
         return session.scalar(exists().where(User.nick == nickname).select())
+
+def get_user_by_id(user_id):
+    with Session(engine) as session:
+        statement = select(User).filter_by(id=user_id)
+        user_obj = session.scalars(statement).first()
+        return user_obj
+
+def get_user_by_nickname(nickname):
+    with Session(engine) as session:
+        statement = select(User).filter_by(nick=nickname)
+        user_obj = session.scalars(statement).first()
+        return user_obj
+
 
 @app.get("/")
 def hello_world():
@@ -37,4 +57,17 @@ def register():
 
 @app.post("/login")
 def login():
-   pass 
+    credentials = request.json
+    user = get_user_by_nickname(credentials["nickname"])
+    if user is None:
+        response = {"message": "Incorrect nickname"}
+        return response, 400
+    if check_password_hash(user.password, credentials["password"]):
+        load_user(user.id)
+        response = {"message": "Successfully loged in"}
+        return response, 200
+    else:
+        response = {"message": "Incorrect password"}
+        return response, 400
+
+
--- backend/user.py
+++ backend/user.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask_login import UserMixin
 
 class Base(DeclarativeBase):
 	pass
 
-class User(Base):
+class User(Base, UserMixin):
     __tablename__ = "User"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     nick: Mapped[str] = mapped_column()
