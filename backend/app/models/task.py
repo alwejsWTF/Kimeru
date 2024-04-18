b17846751b7835fbb899350e2bd2aa5c925e1f2c
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2024-04-19 01:16:43 +0200

    Implement `Task` entity

---
+++ backend/app/models/task.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Task(Base):
+    __tablename__ = "task"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    description: Mapped[str] = mapped_column()
