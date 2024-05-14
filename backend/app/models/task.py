dded11badb4ab1ff86c7afe4395834d8cc18b7f9
murban11 <milosz.urbaniak02@gmail.com>
2024-05-14 23:47:23 +0200

    Add `name` and `points` to `Task` class entity

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -8,4 +8,6 @@ class Base(DeclarativeBase):
 class Task(Base):
     __tablename__ = 'task'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column()
+    points: Mapped[int] = mapped_column()
